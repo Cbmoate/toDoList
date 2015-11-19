64d29066727385708cd15b7ad4f1c729e8ae61da
CB <cbmoate@gmail.com>
2015-11-18 20:32:18 -0500

    first wave of work

---
+++ java/buttons.js
@@ -0,0 +1,6 @@
+$(document).ready(function()){
+  var newRow = $("<tr>");
+  var 
+  var deleteBtn = $("<button>").addClass("btn btn-danger").append("Delete");
+  var deleteTd = $("<td>").append(deleteBtn);
+}
